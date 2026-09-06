fcfb6776da06012352a62a841c46c8b178d274e9
Muhammad Arif <92arifawan@gmail.com>
2026-09-06 19:40:10 +0500

    fix: remove vite from package.json completely

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['electron-store'] // ye line nayi add karo
-  }
-})
